e736ccf0fbbda5fa7c349ce1480e1a7b07d2241b
Omar M Juarez <omarmju906p@hotmail.com>
2020-05-18 19:30:47 -0500

    Se agrega destructuración de datosl.

--- src/es6/index.js
+++ src/es6/index.js
@@ -17,3 +17,21 @@ obtenerDatosES6();
 obtenerDatosES6("Valeria", 25, "MX");
 
 
+/** DESTRUCTURACIÓN DE DATOS **/
+const persona = {
+    nombre: "Jhovana",
+    edad: 27,
+    pais: "MX"
+};
+
+const { nombre, edad, pais } = persona;
+console.log(nombre, edad, pais);
+
+
+/** SPREAD OPERATOR **/
+const chicasGuapas = ["Jhivana", "Sharon", "Viridiana", "Joseline", "Valeria"];
+const profesiones = ["Doctora", "Diseñadora", "Ingeniera", "Desarrolladora", "Abogada"];
+const team = ["Leslie", ...chicasGuapas, ...profesiones];
+console.log(team);
+
+
