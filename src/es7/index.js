737c132cd925cc86d4db9ea58b00a79f6ef13b3f
Omar M Juarez <omarmju906p@hotmail.com>
2020-05-21 18:36:27 -0500

    Se agrega archivo de características de ES7.

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+// Inclides para arreglos en ES7.
+let chicas = ["Jhovana", "Valeria", "Viridiana", "Sharon", "Daniela", "Jenifer"];
+
+if (chicas.includes("Valeria")) {
+    console.log("Mi novia está aqui 😍");
+} else {
+    console.log("Mi novia no está aqui 😩");
+}
+
+// Exponentes en ES7
+let base = 4;
+let exponente = 3;
+console.log(`El numero ${base} elevado a la potencia ${exponente} es igual a ${base ** exponente}.`);
